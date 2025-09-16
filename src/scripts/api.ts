57178bd6e623f5213c25011fd1c0d9631de85d3c
mhoa404 <mhoa.workspace@gmail.com>
2025-09-16 21:00:03 +0700

    Change backend API

--- src/scripts/api.ts
+++ src/scripts/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { removeToken, getToken, saveToken } from "../utils/auth";
 
-export const BACKEND_API = "https://backend-zma.up.railway.app";
+export const BACKEND_API = "https:backend-zalo.mhoa.id.vn";
 
 const api = axios.create({
   baseURL: BACKEND_API,
